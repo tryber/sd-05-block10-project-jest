023cac85c12dc1ff6be41cf49acaa79384e738ec
César Alves <alvescesar@hotmail.com>
2020-07-07 16:03:08 -0300

    Phone call answered

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,13 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
     // Insira seu teste assíncrono aqui
+    const hello = await answerPhone(true);
+    expect(hello).toEqual('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
+  test("ocupado", async () => {
     // Insira seu teste assíncrono aqui
+    try {
+      await answerPhone(false);
+    } catch(error) {
+      expect(error).toEqual('Infelizmente não podemos atender...');
+    }
   });
 });
-// primeiro commit
