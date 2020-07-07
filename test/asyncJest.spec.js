5b8c7553508f032eec074ae24f58be2e67b093a1
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-07 16:41:32 -0300

    Implementação asyncJest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    const callReturn = await answerPhone(true);
+    expect(callReturn).toBe('Oi!');
   });
-  test("ocupado", () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false);
+    } catch (erro) {
+    expect(erro).toBe('Infelizmente não podemos atender...');
+    }
   });
 });
