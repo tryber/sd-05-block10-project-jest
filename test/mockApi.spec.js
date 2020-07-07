0c3a96527465aa772a74d345126528df25343071
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-07 18:06:48 -0300

    Easy Peasy lemon Squeezy setupTearDown.spec.js

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -27,15 +27,15 @@ describe('verifica o usuário', () => {
   gender:'male',
   name: {
     first:'Antônio',
-    last:'Britto'
+    last:'Britto',
   },
   location: {
-    country:'Brazil'
+    country:'Brazil',
   },
   email:'tunico@bol.com.br',
   login: {
     username:'tunicao123',
-    password:'1234567890'
+    password:'1234567890',
   },
   }
 
