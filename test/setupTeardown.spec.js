dfc76e0e6cc58d93ad5d526e91e2442ec47afa63
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-07 19:23:39 -0300

    Implementando o teste do setupTearDown.

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,5 @@
 const adventure = require('../src/setupTeardown');
+const { randomAttack, specialists } = adventure;
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -21,7 +22,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  beforeEach(() => (randomAttack()));
+  afterEach(() => console.log(specialists.map(({ nome, classe }) =>
+  `${nome} - The ${classe}`).join(', ')));
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
