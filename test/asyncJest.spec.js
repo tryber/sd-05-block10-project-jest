23d9cf7b543028707e0b10d6f459682a48f08bd7
Felipe Vieira <flpvieira86@gmail.com>
2020-07-07 10:55:58 -0300

    asyncJest test

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    const trueReturn = await answerPhone(true);
+    expect(trueReturn).toBe('Oi!');
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
-    assert.fail();
     // Insira seu teste assíncrono aqui
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    }
   });
 });
