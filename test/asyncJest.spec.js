253fa2264850dd81bbe784d19cbf4220f26f9bcd
RenataNovais <rnovaiscs@gmail.com>
2020-07-07 17:26:51 -0300

    First commit! :rocket: requirement 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const answerPhone = require("../src/asyncJest");
+const answerPhone = require('../src/asyncJest');
 
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -10,13 +10,22 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+describe('o retorno do telefonema', () => {
+  // test('atende', async () => {
+  //   const atendeu = await answerPhone(true);
+  //   expect(atendeu).toBe('Oi!');
+  // });
+  // test('ocupado', async() => {
+  //   try {
+  //     await answerPhone(false);
+  //   } catch (e) {      
+  //     expect(e).toEqual('Infelizmente não podemos atender...');
+  //   }
+  // });
+  test('atende', () => {
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', () => {
+    expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
