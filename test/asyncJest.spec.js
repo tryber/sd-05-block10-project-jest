dace48b4641ff1106b423a12c78e5b8df6bc33a3
Larissa Palombo <larissampalombo@gmail.com>
2020-07-07 15:41:26 -0300

    inicio primeiro teste

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    const ok = await answerPhone(true);
+    expect(ok).toBe('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toBe('Infelizmente não podemos atender...');
+    }
+  }); 
 });
