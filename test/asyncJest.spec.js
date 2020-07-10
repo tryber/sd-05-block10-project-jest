889fc2db9c24bdb02dc9912a11d4cbc546e79446
pauloricardoz <trybe.przt@gmail.com>
2020-07-10 18:48:56 -0300

    first exercise

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,7 @@
 const answerPhone = require("../src/asyncJest");
-
+const teste = {
+  answerPhone
+};
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,12 +13,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
     // Insira seu teste assíncrono aqui
+    return await answerPhone(true).then(resp => resp);
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false).then(resp => resp)
+    } catch (e) {
+      expect(e).toBe('Infelizmente não podemos atender...')
+    }
+
   });
 });
