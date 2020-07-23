aad7f35a242d28c870780842f898c3d2f983ee1f
SamuelPPlab <samuelpp.s@hotmail.com>
2020-07-23 00:22:35 -0300

    primeiro requisito 'asynctest'

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    return answerPhone('Alô').then( response =>
+      expect(response).toBe('Oi!')
+    )
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone()
+    }
+    catch (error) {
+      expect(error).toEqual('Infelizmente não podemos atender...')
+    }
   });
 });
