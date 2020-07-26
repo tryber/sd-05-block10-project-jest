f7b347d7c3bd3f8082e545a46e7b73cc3dc5bb39
Cass007 <thecastieel07@gmail.com>
2020-07-26 20:03:00 -0300

    iniciando

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -35,7 +35,7 @@ describe("verifica o usuário", () => {
   api.fetchURL = jest.fn().mockResolvedValue(nomeSelecionado);
   test("verifica se o usuário é o tunico", async () => {
     return api.fetchURL().then((user) => {
-      expect(user.gender).toEqual("male");
+      expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual("Antônio");
       expect(user.name.last).toEqual("Britto");
       expect(user.location.country).toEqual("Brazil");
