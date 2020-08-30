6462f25ca70e3e4d8420a19a47f8b1cb3ad80be7
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-30 09:17:55 -0300

    asyncJest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
+    //assert.fail();
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(true)).resolves.toEqual("Oi!");
   });
-  test("ocupado", () => {
-    assert.fail();
+  test("ocupado", async () => {
+    //assert.fail();
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects.toEqual(
+      "Infelizmente não podemos atender..."
+    );
   });
 });
